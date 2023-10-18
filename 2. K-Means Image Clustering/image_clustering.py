1c3c536b7fbf841e2a28f287791c75e72f837a5c
Tran Cuong <tranquoccuong1109@gmail.com>
2023-10-18 14:05:47 +0700

    display new img

--- 2. K-Means Image Clustering/image_clustering.py
+++ 2. K-Means Image Clustering/image_clustering.py
@@ -1,19 +1,30 @@
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import numpy
 
-img = matplotlib.pyplot.imread("a.jpg")
+img = plt.imread("D:/Teaching/AI_basic/2. K-Means Image Clustering/a.jpg")
 
 width = img.shape[0]
 height = img.shape[1]
 
-print(img.shape)
-
 img = img.reshape(width*height,3)
 
-kmeans = KMeans(n_clusters=4).fit(img)
+print(img.shape)
+
+kmeans = KMeans(n_clusters=50).fit(img)
 
 labels = kmeans.predict(img)
 clusters = kmeans.cluster_centers_
 
 print(labels)
 print(clusters)
+
+img2 = numpy.zeros_like(img)
+
+for i in range(len(img2)):
+	img2[i] = clusters[labels[i]]
+
+img2 = img2.reshape(width,height,3)
+
+plt.imshow(img2)
+plt.show()

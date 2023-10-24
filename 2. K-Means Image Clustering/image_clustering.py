736173058b5477280f22b4f83230eee9f123e246
Tran Cuong <tranquoccuong1109@gmail.com>
2023-10-24 20:57:04 +0700

    linear regression code

--- 2. K-Means Image Clustering/image_clustering.py
+++ 2. K-Means Image Clustering/image_clustering.py
@@ -4,14 +4,14 @@ import numpy
 
 img = plt.imread("D:/Teaching/AI_basic/2. K-Means Image Clustering/a.jpg")
 
-width = img.shape[0]
-height = img.shape[1]
+height = img.shape[0]
+width = img.shape[1]
 
 img = img.reshape(width*height,3)
 
 print(img.shape)
 
-kmeans = KMeans(n_clusters=50).fit(img)
+kmeans = KMeans(n_clusters=5).fit(img)
 
 labels = kmeans.predict(img)
 clusters = kmeans.cluster_centers_
@@ -24,7 +24,7 @@ img2 = numpy.zeros_like(img)
 for i in range(len(img2)):
 	img2[i] = clusters[labels[i]]
 
-img2 = img2.reshape(width,height,3)
+img2 = img2.reshape(height,width,3)
 
 plt.imshow(img2)
 plt.show()
